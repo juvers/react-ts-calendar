06af53ec062a7cfa39b34e9abdd8e15751bc621c
Jude K <judekuti@gmail.com>
2021-03-20 06:37:25 -0500

    Reserved one sampele of React.FC

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+// import DemoContainer from "./App";
+import Calendar from './App';
+interface I18n {
+  previousMonth: string;
+  nextMonth: string;
+  months: string[];
+  weekdays: string[];
+  weekdaysShort: string[];
+}
+
+const i18nz: I18n = {
+  previousMonth: "Previous month",
+  nextMonth: "Next month",
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  weekdays: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  weekdaysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="wrapper">
+      {/* <DemoContainer i18n={i18nz} /> */}
+      <Calendar i18n={i18nz} />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
